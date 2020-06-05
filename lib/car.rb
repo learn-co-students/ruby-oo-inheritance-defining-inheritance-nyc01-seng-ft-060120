f88288e2ab9a43a82584c8aa4633f61f1a5632ad
mayalexandera <maya.blevins@gmail.com>
2020-06-05 18:57:29 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,13 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+
+  def initialize(wheel_size, wheel_number)
+    super(wheel_size, wheel_number)
+  end
+
+  def go
+    return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+  end
+
+end
